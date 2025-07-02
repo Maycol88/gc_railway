b0ab689ce960a83898fc74f715d911f234bb880c
Maycol88 <maycol.amaral2014@gmail.com>
2025-07-01 22:16:07 -0300

    fix: usar API do Render em produção

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+// File: index.js
 const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
+
 
 app.use(cors());
 
@@ -12,5 +14,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
